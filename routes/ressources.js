7a9ba1fcec034a422f8d65bb7fc3755a6680497b
Mike Pourbaix <mike.pourbaix@student.vinci.be>
2025-09-25 19:38:36 +0200

    add: blog

--- routes/ressources.js
+++ routes/ressources.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('ressources.hbs');
+const RSSParser = require('rss-parser');
+const parser = new RSSParser;
+
+router.get('/', async (req, res) => {
+  try {
+    const feed = await parser.parseURL("https://www.lecho.be/rss/fonds.xml");
+
+    const articles = feed.items.map(item => ({
+      title: item.title,
+      summary: item.contentSnippet || item.content || "",
+      link: item.link,
+      image: item.enclosure?.url || (item['media:content'] ? item['media:content'].url : null)
+    }));
+
+    res.render('ressources.hbs', { articles, title: "Ressources / Blog" });
+  } catch (err) {
+    console.error("Erreur lors du chargement du flux RSS :", err);
+    res.render('ressources.hbs', { articles: [], title: "Ressources / Blog" });
+  }
 });
 
 
